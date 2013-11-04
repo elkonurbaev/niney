221ff8c5be19d0575530588f6e49fdedb9335f87
elkonurbaev <macbookpro@Macbooks-MacBook-Pro.local>
2013-11-04 18:10:04 +0600

    css style match the website www.twentemobiel.nl, added default info text on right panel.

--- source/libs/nine-e/nine-e.js
+++ source/libs/nine-e/nine-e.js
@@ -100,6 +100,7 @@ angular.module('nine-e', ['monospaced.mousewheel']).
             },
             controller: ['$scope', function ($scope) {
                 this.scope = $scope;
+                this.scope.curServiceModel = null;
             }],
             compile: function (element, attr, transclude) {
                 return function ($scope, $element, $attr, $parentCtrl) {
@@ -109,8 +110,14 @@ angular.module('nine-e', ['monospaced.mousewheel']).
                     $parentCtrl.scope.$watch('serviceModel', function(val) { $scope.serviceModel = val; });
                     
                     var childElement, childScope;
+                    var defaultInfoText = document.getElementById('infoPanel').innerHTML;
                     $scope.$watch('layer.visible', function(val) {
                         if (childElement) {
+                        	if($scope.curServiceModel != null) {
+                        		if($scope.curServiceModel.id == $scope.layer.id) {
+                        			document.getElementById('infoPanel').innerHTML = defaultInfoText;
+                        		}
+                        	}
                             $element.contents().remove();
                             childElement = undefined;
                         }
@@ -121,7 +128,7 @@ angular.module('nine-e', ['monospaced.mousewheel']).
                         if (val) {
                             childScope = $scope.$new();
                             transclude(childScope, function (clone) {
-                                childElement = clone;
+                            	childElement = clone;
                                 $element.append(clone);
                             });
                         }
@@ -161,12 +168,6 @@ angular.module('nine-e', ['monospaced.mousewheel']).
                 };
                 $scope.showInfo = function(feature){
                 	console.log('showInfo A ' + feature.propertyValues[1]);
-                	/*console.log(feature.propertyValues[2]);
-                	console.log(feature.propertyValues[4]);
-                	console.log(feature.propertyValues[5]);
-                	console.log(feature.propertyValues[6]);*/
-                	//document.getElementById('infoPanel').append();
-                
                 }
             }],
             link: function($scope, $element, $attr, $parentCtrl) {
@@ -193,9 +194,10 @@ angular.module('nine-e', ['monospaced.mousewheel']).
                 assetPropertyIndex: '@assetpropertyindex',
                 asset: '@asset'
             },
-            controller: ['$scope', function ($scope) {
+            controller: ['$scope', function($scope){
             	$scope.prevElement = null;
             	$scope.сurElement = null;
+            	$scope.curServiceModel = null;
                 $scope.isInsideBoundaries = function(item) {
                 	var itemEnvelope = item.propertyValues[$scope.propertyIndex].getEnvelope();
                 	return itemEnvelope.intersects($scope.envelopeModel.getEnvelope());
@@ -207,25 +209,23 @@ angular.module('nine-e', ['monospaced.mousewheel']).
                 	if($scope.сurElement != event.target)  event.target.className = 'ng-scope'; 
                 }
                 $scope.showInfo = function(feature, event) {
-                	$scope.сurElement = event.target;
+               	 	$scope.сurElement = event.target;
                 	if($scope.prevElement != null) $scope.prevElement.className = 'ng-scope';
                 	event.target.className = event.target.className + ' highlightSymbolizer';
                 	$scope.prevElement = event.target;
-                	
                 	var domInfoPanel = document.getElementById('infoPanel');
                		domInfoPanel.innerHTML = '';
-               		//console.log('showInfo B ' + feature.propertyValues[1]);
-                	var curServiceModel = this.getServiceByName(feature.featureType.name);
-                	if (curServiceModel != null) {
-                		var fields = (curServiceModel.infoFieldsToInclude.replace(' ', '')).split(',');
-                		if (curServiceModel.selectionCommand == 'all') {
+                	$scope.curServiceModel = this.getServiceByName(feature.featureType.name);
+                	if ($scope.curServiceModel != null) {
+                		var fields = ($scope.curServiceModel.infoFieldsToInclude.replace(' ', '')).split(',');
+                		if ($scope.curServiceModel.selectionCommand == 'all') {
                 			var k = 0;
                 			for (var i = 0; i < fields.length; ++i) {
                 				var html = feature.propertyValues[fields[i]];
                 				if(html != null){
-                					//console.log('html fields[i]: '+ fields[i] + ' ' + html);
+      
                 					if(this.isURL(html)) {
-                						var textLink = (curServiceModel.customLinkTitles != null) ? curServiceModel.customLinkTitles[k] : 'link';
+                						var textLink = ($scope.curServiceModel.customLinkTitles != null) ? $scope.curServiceModel.customLinkTitles[k] : 'link';
                 						html = this.createLinkTag(html, textLink);
                 						++k;
                 					} else {
@@ -234,7 +234,7 @@ angular.module('nine-e', ['monospaced.mousewheel']).
                 					domInfoPanel.appendChild(html);
                 				}
                 			}
-                		} else if (curServiceModel.selectionCommand == 'url') {
+                		} else if ($scope.curServiceModel.selectionCommand == 'url') {
                 			window.open(feature.propertyValues[fields[0]]);
                 		} 
                 	}
@@ -284,6 +284,11 @@ angular.module('nine-e', ['monospaced.mousewheel']).
                 $parentCtrl.scope.$watch('featureModel', function(val) { $scope.featureModel = val; });
                 $parentCtrl.scope.$watch('envelopeModel', function(val) { $scope.envelopeModel = val; });
                 $parentCtrl.scope.$watch('serviceModel', function(val) { $scope.serviceModel = val; });
+                $scope.$watch('prevElement', function(val) { 
+                	if($scope.curServiceModel != null) {
+                		$parentCtrl.scope.curServiceModel = $scope.curServiceModel;
+                	}
+                });
                 $attr.$observe('maxscale', function(val) { $scope.maxScale = angular.isDefined(val) ? val : Number.MAX_VALUE; });
             }
         };
@@ -291,7 +296,7 @@ angular.module('nine-e', ['monospaced.mousewheel']).
     }).
     directive('geometryimagesymbolizer', function factory() {
         var directiveDefinitionObject = {
-            template: '<div class="mapfeaturelayer" ng-if="maxScale >= focusModel.centerScale.scale"><div ng-repeat="feature in featureModel.features" ng-click="showInfo(feature)"><img ng-repeat="geometry in feature.propertyValues[propertyIndex].geometries | filter:isInsideBoundaries track by $index" src="{{asset.replace(\'$\', feature.propertyValues[assetPropertyIndex])}}" style="position: absolute; top: {{focusModel.centerScale.getPixY(boundsModel.bounds.height, geometry.y)}}px; left: {{focusModel.centerScale.getPixX(boundsModel.bounds.width, geometry.x)}}px" /></div></div>',
+            template: '<div class="mapfeaturelayer" ng-if="maxScale >= focusModel.centerScale.scale"><div ng-repeat="feature in featureModel.features" ng-mouseover="highlightSymbolizer($event)" ng-mouseout="unhighlightSymbolizer($event)" ng-click="showInfo(feature, $event)"><img ng-repeat="geometry in feature.propertyValues[propertyIndex].geometries | filter:isInsideBoundaries track by $index"  src="{{asset.replace(\'$\', feature.propertyValues[assetPropertyIndex])}}" style="position: absolute; top: {{focusModel.centerScale.getPixY(boundsModel.bounds.height, geometry.y)}}px; left: {{focusModel.centerScale.getPixX(boundsModel.bounds.width, geometry.x)}}px" /></div></div>',
             restrict: 'E',
             require: '^mapfeaturelayer',
             replace: true,
@@ -302,24 +307,102 @@ angular.module('nine-e', ['monospaced.mousewheel']).
                 asset: '@asset'
             },
             controller: ['$scope', function ($scope) {
+            	$scope.prevElement = null;
+            	$scope.сurElement = null;
+            	$scope.curServiceModel = null;
                 $scope.isInsideBoundaries = function(item) {
                 	var itemEnvelope = item.getEnvelope();
                 	return itemEnvelope.intersects($scope.envelopeModel.getEnvelope());
                 };
                 $scope.showInfo = function(feature) {
                 	console.log('showInfo C ' + feature.propertyValues[0]);
-                	/*console.log(feature.propertyValues[2]);
-                	console.log(feature.propertyValues[4]);
-                	console.log(feature.propertyValues[5]);
-                	console.log(feature.propertyValues[6]);*/
-                	//window.open(feature.propertyValues[2]);
                 }
+                $scope.highlightSymbolizer = function(event) {
+                	event.target.className = event.target.className + ' highlightSymbolizer';
+                }
+                $scope.unhighlightSymbolizer = function(event) {
+                	if($scope.сurElement != event.target)  event.target.className = 'ng-scope'; 
+                }
+                $scope.showInfo = function(feature, event) {
+               	 	$scope.сurElement = event.target;
+                	if($scope.prevElement != null) $scope.prevElement.className = 'ng-scope';
+                	event.target.className = event.target.className + ' highlightSymbolizer';
+                	$scope.prevElement = event.target;
+                	var domInfoPanel = document.getElementById('infoPanel');
+               		domInfoPanel.innerHTML = '';
+                	$scope.curServiceModel = this.getServiceByName(feature.featureType.name);
+                	if ($scope.curServiceModel != null) {
+                		var fields = ($scope.curServiceModel.infoFieldsToInclude.replace(' ', '')).split(',');
+                		if ($scope.curServiceModel.selectionCommand == 'all') {
+                			var k = 0;
+                			for (var i = 0; i < fields.length; ++i) {
+                				var html = feature.propertyValues[fields[i]];
+                				if(html != null){
+                					if(this.isURL(html)) {
+                						var textLink = ($scope.curServiceModel.customLinkTitles != null) ? $scope.curServiceModel.customLinkTitles[k] : 'link';
+                						html = this.createLinkTag(html, textLink);
+                						++k;
+                					} else {
+                						html = this.createTag(html, 'span');
+                					}
+                					domInfoPanel.appendChild(html);
+                				}
+                			}
+                		} else if ($scope.curServiceModel.selectionCommand == 'url') {
+                			window.open(feature.propertyValues[fields[0]]);
+                		} 
+                	}
+                };
+                /* option to put them inside root directive, instead of duplicating */
+                $scope.getServiceByName = function(name) {
+                	for(var i = 0; i < $scope.serviceModel.length; ++i) {
+                		if(name == $scope.serviceModel[i].featureName) {
+                			return $scope.serviceModel[i];
+                		}
+                	}
+                	return nil;
+                };
+                $scope.createTag = function(val, type='span', newLine=true) {
+                	var tag = document.createElement(type);
+                	var textWithBreaks = val.split(/\[0A\]/);
+                	var a_link = val.substring(val.indexOf('[link]')+('[link]').length, val.indexOf('[text]'));
+                	var a_text = val.substring(val.indexOf('[text]')+('[text]').length, val.indexOf('[end]'));
+                	//var regex = new RegExp(/(\[link\])(.*?)(\[text\])/g);
+                	//var ss = val.match(regex);
+                	if(this.isURL(a_link) && a_text != '') {
+                		return this.createLinkTag(a_link, a_text);
+                	}
+                	for(var i = 0; i < textWithBreaks.length; ++i) {
+                		tag.appendChild(document.createTextNode(textWithBreaks[i]));
+                		if((i+1) != textWithBreaks.length) tag.appendChild(document.createElement('br'));
+                	}
+                	(newLine) ? tag.appendChild(document.createElement('br')) : null;
+                	return tag;
+                };
+                $scope.createLinkTag = function(val, text, newLine=true) {
+                	var tag = document.createElement('a');
+                	tag.appendChild(document.createTextNode(text));
+                	tag.href = val;
+                	tag.target = "_blank";
+                	(newLine) ? tag.appendChild(document.createElement('br')) : null;
+                	return tag;
+                };
+                $scope.isURL = function(str) { 
+               		var regexp = new RegExp("^s?https?:\/\/[-_.!~*'()a-zA-Z0-9;\/?:\@&=+\$,%#]+$");
+               		return regexp.test(str);
+				}
             }],
             link: function($scope, $element, $attr, $parentCtrl) {
                 $parentCtrl.scope.$watch('boundsModel', function(val) { $scope.boundsModel = val; });
                 $parentCtrl.scope.$watch('focusModel', function(val) { $scope.focusModel = val; });
                 $parentCtrl.scope.$watch('featureModel', function(val) { $scope.featureModel = val; });
                 $parentCtrl.scope.$watch('envelopeModel', function(val) { $scope.envelopeModel = val; });
+                $parentCtrl.scope.$watch('serviceModel', function(val) { $scope.serviceModel = val; });
+                $scope.$watch('prevElement', function(val) { 
+                	if($scope.curServiceModel != null) {
+                		$parentCtrl.scope.curServiceModel = $scope.curServiceModel;
+                	}
+                });
                 $attr.$observe('maxscale', function(val) { $scope.maxScale = angular.isDefined(val) ? val : Number.MAX_VALUE; });
             }
         };
